c7bc3a65bf04104b53c1960a605c9e63f76ca390
mgt <mgt@oi-wiki.org>
2026-01-14 00:55:40 +0800

    init soft device

--- firmware/memory.x
+++ firmware/memory.x
@@ -1,9 +1,8 @@
-/* memory.x */
 MEMORY
 {
-  /* No SoftDevice: app starts at 0x00000000 */
-  FLASH : ORIGIN = 0x00000000, LENGTH = 1024K
+  /* S140 7.3.0 occupies up to 0x00026498, round up to 0x00027000 */
+  FLASH : ORIGIN = 0x00027000, LENGTH = 1024K - 0x27000
 
-  /* No SoftDevice: full RAM available */
-  RAM : ORIGIN = 0x20000000, LENGTH = 256K
+  /* Reserve 0x3000 RAM for SoftDevice by default */
+  RAM : ORIGIN = 0x20003000, LENGTH = 256K - 0x3000
 }
--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -6,12 +6,29 @@ use embassy_nrf::gpio::{Level, Output, OutputDrive};
 use embassy_time::Timer;
 
 use {defmt_rtt as _, panic_probe as _};
+use nrf_softdevice::Softdevice;
+
+#[embassy_executor::task]
+async fn softdevice_task(sd: &'static Softdevice) {
+    sd.run().await
+}
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let mut config = embassy_nrf::config::Config::default();
     config.lfclk_source = embassy_nrf::config::LfclkSource::InternalRC;
+
+    {
+        use embassy_nrf::interrupt::Priority;
+
+        config.gpiote_interrupt_priority = Priority::P2;
+        config.time_interrupt_priority = Priority::P2;
+    }
+
     let p = embassy_nrf::init(config);
+
+    let sd = Softdevice::enable(&Default::default());
+    spawner.spawn(softdevice_task(sd)).unwrap();
 
     // LED is on P0.15 per promicro_diy variant.
     let mut led = Output::new(p.P0_15, Level::Low, OutputDrive::Standard);
