66235f2fffea3e80194695bad86702dc2b3ac923
mgt <mgt@oi-wiki.org>
2026-01-13 23:10:49 +0800

    init

--- firmware/memory.x
+++ firmware/memory.x
@@ -1,12 +1,9 @@
 /* memory.x */
 MEMORY
 {
-  /* S140 v7.x.x 大约占用 152KB Flash (0x26000) */
-  /* 应用代码从 0x26000 开始 */
-  FLASH : ORIGIN = 0x00026000, LENGTH = 1024K - 0x26000
+  /* No SoftDevice: app starts at 0x00000000 */
+  FLASH : ORIGIN = 0x00000000, LENGTH = 1024K
 
-  /* SoftDevice 也占用一部分 RAM，具体取决于蓝牙配置 */
-  /* 这里预留 0x3000 (12KB) 给 SoftDevice，通常够用 */
-  /* 如果运行时报错 "softdevice RAM check failed"，你需要增大 ORIGIN 并减小 LENGTH */
-  RAM : ORIGIN = 0x20003000, LENGTH = 256K - 0x3000
+  /* No SoftDevice: full RAM available */
+  RAM : ORIGIN = 0x20000000, LENGTH = 256K
 }
--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_nrf::gpio::{Level, Output, OutputDrive};
+use embassy_time::Timer;
+
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let mut config = embassy_nrf::config::Config::default();
+    config.lfclk_source = embassy_nrf::config::LfclkSource::InternalRC;
+    let p = embassy_nrf::init(config);
+
+    // LED is on P0.15 per promicro_diy variant.
+    let mut led = Output::new(p.P0_15, Level::Low, OutputDrive::Standard);
+
+    loop {
+        led.set_high();
+        defmt::info!("LED ON");
+        Timer::after_millis(100).await;
+        led.set_low();
+        defmt::info!("LED OFF");
+        Timer::after_millis(100).await;
+    }
 }
